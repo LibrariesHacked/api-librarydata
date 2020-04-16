d762c283bb2aaf165d198332f186ffe07a94f8a8
Dave <DaveBathnes@users.noreply.github.com>
2020-04-16 18:03:39 +0100

    Starting work on services video feed

--- routes/services.js
+++ routes/services.js
@@ -11,10 +11,17 @@ router.get('/airtable/:service_code', (req, res) => {
 });
 
 /**
- * 
  * Gets all the library services from airtable
  */
 router.get('/airtable', (req, res) => {
+  airtableHelper.getAllRecordsInTable(process.env.AIRTABLE_LIBRARY_SERVICES_BASE_ID, process.env.AIRTABLE_LIBRARY_SERVICES_TABLE_NAME, (data) => { res.json(data) });
+});
+
+/**
+ * Produces a single video feed from all existing feeds.
+ * Cached to speed things up
+ */
+router.get('/airtable/feeds/video', (req, res) => {
   airtableHelper.getAllRecordsInTable(process.env.AIRTABLE_LIBRARY_SERVICES_BASE_ID, process.env.AIRTABLE_LIBRARY_SERVICES_TABLE_NAME, (data) => { res.json(data) });
 });
 
