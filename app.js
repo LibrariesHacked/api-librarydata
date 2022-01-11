16154d71adb5795481754415ec7d0406c301bda4
Dave <8709533+DaveBathnes@users.noreply.github.com>
2022-01-11 22:04:20 +0000

    #16 Also allowing Authorization request header

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ require('dotenv-defaults').config()
 // Allow cross origin
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
   res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS')
   next()
 })
